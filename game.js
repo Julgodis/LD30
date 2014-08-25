2b1befa211b9c9f2c5cadb0d6cf1e21663e499e5
Julgodis <jonathan@familjenwase.se>
2014-08-25 07:30:38 +0200

    Add Google Analytics

--- game.js
+++ game.js
@@ -49,11 +49,11 @@ function OpenInNewTab() {
 	window.location = "http://www.ludumdare.com/compo/ludum-dare-30/?action=preview&uid=16119";
 }
 
-/*
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-39491934-3', 'github.com');
-ga('send', 'pageview');*/
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-39491934-4', 'auto');
+  ga('send', 'pageview');
